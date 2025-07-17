8fb6545b5b9f9b9a2fce1573268be74a4660b5a9
vennela <gvennelaraju@gmail.com>
2025-07-17 17:42:53 +0000

    latest

--- main.tf
+++ main.tf
@@ -12,10 +12,19 @@ resource "azurerm_virtual_network" "vnet" {
   location            = azurerm_resource_group.casestudy.location
   resource_group_name = azurerm_resource_group.casestudy.name
 }
+
+
 resource "azurerm_storage_account" "vennelastorage1345" {
     name                     = "vennelastorage1345"
     resource_group_name      = azurerm_resource_group.casestudy.name
     location                 = azurerm_resource_group.casestudy.location
     account_tier             = "Standard"
     account_replication_type = "LRS"
+}
+
+
+resource "azurerm_storage_container" "vennelablob" {
+  name ="vennelablob"
+  storage_account_name ="azurerm_storage_account.vennelastorage1345.name"
+  container_access_type="private"
 }
