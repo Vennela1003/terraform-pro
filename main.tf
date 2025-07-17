4d9afdac3848abf38f98372c0642ee195fc2bec6
vennela <gvennelaraju@gmail.com>
2025-07-17 11:31:18 +0000

    latest

--- main.tf
+++ main.tf
@@ -6,3 +6,10 @@ resource "azurerm_resource_group" "casestudy" {
   name     = "casestudy"
   location = "East US"
 }
+resource "azurerm_storage_account" "vennelacasestudy143" {
+    name                     = "vennelacasestudy134"
+    resource_group_name      = azurerm_resource_group.casestudy.name
+    location                 = azurerm_resource_group.casestudy.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+}
