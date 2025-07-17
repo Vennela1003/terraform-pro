781ab156773ec9311a5de96625082f8a26ca2fd2
vennela <gvennelaraju@gmail.com>
2025-07-17 11:40:11 +0000

    latest

--- main.tf
+++ main.tf
@@ -6,10 +6,9 @@ resource "azurerm_resource_group" "casestudy" {
   name     = "casestudy"
   location = "East US"
 }
-resource "azurerm_storage_account" "vennelacasestudy143" {
-    name                     = "vennelacasestudy134"
-    resource_group_name      = azurerm_resource_group.casestudy.name
-    location                 = azurerm_resource_group.casestudy.location
-    account_tier             = "Standard"
-    account_replication_type = "LRS"
+resource "azurerm_virtual_network" "vnet" {
+  name                = "example-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.casestudy.location
+  resource_group_name = azurerm_resource_group.casestudy.name
 }
